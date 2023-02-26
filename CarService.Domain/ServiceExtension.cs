a2a113eaeb95ec5f072b905655f7c43ccd377f98
Rinat050 <safiullin.rinat.05@gmail.com>
2023-02-26 20:19:41 +0300

    add Blazor project

---
+++ CarService.Domain/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using CarService.Domain.Services;
+using CarService.Domain.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarService.Domain
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAuthorizationService, AuthorizationService>();
+
+            return services;
+        }
+    }
+}
