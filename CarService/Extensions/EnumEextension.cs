f775296cf7310ff12bd66ef51e00ad2e6961fc42
Rinat050 <safiullin.rinat.05@gmail.com>
2023-03-11 22:17:52 +0300

    add users page

---
+++ CarService/Extensions/EnumEextension.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace CarService.Extensions
+{
+    public static class EnumEextension
+    {
+        public static string? GetDisplayName(this Enum type)
+        {
+            return type.GetType().GetField(type.ToString())?.GetCustomAttribute<DisplayAttribute>()?.Name;
+        }
+    }
+}
