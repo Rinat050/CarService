f883b5fe68b8b63faf6a0e2f4328163a5a15eb1f
Rinat050 <safiullin.rinat.05@gmail.com>
2023-03-23 22:01:35 +0300

    add createPurchaseOrder Dialog and orders page

---
+++ CarService.Domain/Models/PurchaseOrderTableItem.cs
@@ -0,0 +1,13 @@
+﻿using CarService.Domain.Enums;
+
+namespace CarService.Domain.Models
+{
+    public class PurchaseOrderTableItem
+    {
+        public string PurchaseOrderId { get; set; }
+        public string Client { get; set; }
+        public string Car { get; set; }
+        public int TotalCost { get; set; }
+        public OrderStatus Status { get; set; } 
+    }
+}
---
+++ CarService/Data/CreatePurchaseOrderCredentials.cs
@@ -0,0 +1,12 @@
+﻿using CarService.Domain.Models;
+
+namespace CarService.Data
+{
+    public class CreatePurchaseOrderCredentials
+    {
+        public Client Client { get; set; }
+        public Car Car { get; set; }
+        public User Diagnostician { get; set; }
+        public User Mechanic { get; set; }
+    }
+}
